2b4007c8cc77d027afde2058a85c5a6eb3054d91
Reimar Rosas <reimarrosas0208@gmail.com>
2021-03-28 14:06:02 -0400

    Added a container for the cards instead of just in the main element, and handled the buttons

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 "use strict";
+
+const slideShowCards = document.querySelectorAll(".slideshow-container > *");
+const gotoLeft = document.querySelector(".btn-left");
+const gotoRight = document.querySelector(".btn-right");
+const randomize = document.querySelector(".btn-random");
+const card = document.querySelector(".card");
+
+let currentCard = 0;
+
+gotoLeft.addEventListener("click", handleGotoLeft);
+
+function handleGotoLeft() {
+  if (currentCard > 0) {
+    --currentCard;
+    slideShowCards[currentCard].scrollIntoView({behavior: "smooth"});
+  }
+}
+
+gotoRight.addEventListener("click", handleGotoRight);
+
+function handleGotoRight() {
+  if (currentCard < slideShowCards.length - 1) {
+    ++currentCard;
+    slideShowCards[currentCard].scrollIntoView({behavior: "smooth"});
+  }
+}
+
+randomize.addEventListener("click", handleRandomize);
+
+function handleRandomize() {
+  let randomNumber = 0;
+  while (randomNumber === currentCard) {
+    randomNumber = Math.floor(Math.random() * slideShowCards.length);
+  }
+  currentCard = randomNumber
+  slideShowCards[currentCard].scrollIntoView({behavior: "smooth"});
+}
